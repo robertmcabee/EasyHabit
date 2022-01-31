2d007b89d6e682fbf428ee6dc909f68624830868
Robert McAbee <robertthemcabee@gmail.com>
2022-01-30 21:10:32 -0700

    add refresh & display functions

--- src/index.js
+++ src/index.js
@@ -1,102 +1,118 @@
 'use strict';
+let dateFNS = require('date-fns');
 
 import { format, differenceInCalendarDays } from 'date-fns';
 
 const form = document.querySelector('form');
 const formName = document.getElementById('form-name');
 const submitButton = document.getElementById('submit-button');
+const habitContainer = document.getElementById('habit-container');
 
 const overlay = document.getElementById('overlay');
 overlay.addEventListener('mousedown', (e) => {
+  // @ts-ignore
   if (e.target.tagName === "DIV") {
-    overlay.style.display = "none"
+    overlay.style.display = "none";
   }
 });
 
 const showOverlayBtn = document.getElementById('show-overlay');
 showOverlayBtn.addEventListener('click', (e) => {
- overlay.style.display = "block"
+  overlay.style.display = "block";
 });
 
 
-const habitArray = [] //stores habit objects in array i.e. 
-// [{name: "anki", id: "0d7611i"}, {name: "stretch", id: "l7pky86"}]
+const habitArray = [];
+//stores habit objects in array i.e. [{name: "anki", id: "0d7611i"}, {name: "stretch", id: "l7pky86"}]
 
 const dayList = { //stores day objects in object
-  // '011722': {
-  //   'fz680z': 100,
-  //   '98sh8a': 0
-  // },
   // '011822': {
   //   'fz680z': 0,
   //   '98sh8a': 0
   // }
-} //stores day objects
+  // '011722': {
+  //   'fz680z': 100,
+  //   '98sh8a': 0
+  // },
+};
 
 submitButton.addEventListener("click", (event) => {
-  event.preventDefault() //prevent reload
-  createHabit(form)
-  form.reset() //reset fields
+  event.preventDefault(); //prevent reload
+  createHabit(form);
+  form.reset(); //reset fields
 });
 
 function createHabit(HTMLFormData) {
-  let formData = new FormData(HTMLFormData) //using formdata to support any number of form inputs without re-writng code
-  formData.append('id', idGen()) //create id
-  const habit = {}
+  let formData = new FormData(HTMLFormData); //using formdata to support any number of form inputs without re-writng code
+  formData.append('id', idGen()); //create id
+  const habit = {};
   for (let key of formData.keys()) { //assign formdata key values to habit object
-    habit[key] = formData.get(key)
-  }
+    habit[key] = formData.get(key);
+  };
   habitArray.push(habit)
   console.log("the current habitArray is:")
   console.log(habitArray)
-  createDayObj(now)
+  displayGrid(dayList, habitArray)
 };
-    
-let dateFNS = require('date-fns');
-
-let testTimeA = new Date(2022, 0, 17); //for debugging purposes
-let testTimeB = new Date(2021, 4, 10); //for debugging purposes
-let testTimeC = new Date(2022, 0, 20); //for debugging purposes
 
 
+let now = new Date();
+
+let testTimeA = new Date(2022, 0, 17); // 1-17-22 for debugging purposes
+let testTimeB = new Date(2022, 0, 29); // 1-29-22 for debugging purposes
+let testTimeC = new Date(2022, 0, 30); // 1-29-22 for debugging purposes
+let testTimeD = new Date(2023, 0, 29); //for debugging purposes
 
 
 function createDayObj(date) {
-  let name = formatDateForObj(date)
-  dayList[name] = {}
+  let name = dateFNS.format(date,'MMddyy')
+  dayList[name] = {date: date}
   habitArray.forEach(habit => { // puts all habits in with a value of 0
     dayList[name][habit.id] = 0 
   });
+};   
+  
+function daysElapsed(currentTime, mostRecentDay) { //returns true if 1+ days have elapsed
+  const numOfDays = dateFNS.differenceInCalendarDays(currentTime, mostRecentDay)
+  const result = []
+  if (numOfDays < 1) return result
+  for (let i = 0; i < numOfDays; i++) {
+    result.unshift(dateFNS.add(mostRecentDay, {days: i + 1})) //adds 1 plus the current index
+  }
+  return result
+  // TODO: add err on negative length 
+};
+
+function refresh(currentTime, mostRecentDay) {
+  const dayArr = daysElapsed(currentTime, mostRecentDay)
+  if (dayArr.length === 0) return //guard
+  for (let i = 0; i < dayArr.length; i++) { //create days
+    createDayObj(dayArr[i])
+  }
   console.log("the current daylist is")
   console.log(dayList)
-};
-
-function loadDays(listOfHabits, listOfDays) {
-  listOfHabits.forEach(element => {
-    
-  });
 }
-    
-let now = new Date();
-    
-const StartDate = now;
-  
-  function hasDayChanged(mostRecentDay, currentTime) { //returns true if 1+ days have elapsed
-  if (dateFNS.differenceInCalendarDays(mostRecentDay, currentTime) > 0) { 
-    return true //new day will need to be displayed
-  } else {
-    return false //no new day needed
-  }
-};
 
-function update(params) { //TODO 
-  // check if new days needed
-    // hasDayChanged(mostRecentDay, currentTime)
-  // create new days
-    // createDayObj(date)
-  // load habits into all days
-  // display habit grid
-}      
+refresh(now, testTimeA)
+displayGrid(dayList, habitArray)
+
+function displayGrid(dayList, habitArray) {
+  let result = ''
+  const rows = (Object.keys(dayList)).length
+  const columns = habitArray.length
+  const openingTemplate = `<section class="row">`
+  const closingTemplate = `</section>`
+  const columnTemplate = `<div class="column"></div>`
+  for (let i = 0; i < rows; i++) {
+    result += openingTemplate
+    result += createDateHTMLTemplate(Object.values(dayList)[i]['date'])
+    for (let j = 0; j < columns; j++) {
+      result += columnTemplate
+    }
+    result += closingTemplate
+  }
+  habitContainer.innerHTML = result
+}
 
 const idGen = () => {
   return Math.random().toString(36).substr(2, 7); //creates a string like "fz680z"
@@ -107,15 +123,13 @@ function createDateHTMLTemplate(day) {
   let dayOfMonth = dateFNS.format(day, 'd') //i.e. 'Monday'
   let dayOfWeek = dateFNS.format(day, 'iiii') //i.e. '18'
   let templateString =
-  `<span class="day-of-week">${dayOfWeek},</span>
-  <span class="date">${month} ${dayOfMonth}</span>`
+  `<div class="column">
+  <span class="day-of-week">${dayOfWeek},</span>
+  <span class="date">${month} ${dayOfMonth}</span>
+  </div>`
   return templateString
 };
 
-function formatDateForObj(date) {
-  return dateFNS.format(date,'MMddyy') //returns string in MMDDYY format, i.e. '120520'
-};
-    
 // const tasks = {
 //   011822: {
 //     habit_123aax: true,
@@ -126,17 +140,3 @@ function formatDateForObj(date) {
 //     habit_653zdz: 100,
 //   }
 // };
-    
-
-  
-
-
-
-    
-
-    
-    
-    
-    
-    
-    
