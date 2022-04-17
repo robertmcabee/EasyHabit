44398ff92c60f5328b7d86ad322d915c1df53689
Robert McAbee <robertthemcabee@gmail.com>
2022-04-16 18:08:36 -0600

    add fade in & drop down animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,35 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        dropin: 'dropin 300ms ease-out',
+        fadein: 'fadein 300ms ease-in-out',
+      },
+      keyframes: {
+
+        dropin: {
+          '0%': {
+            transform: 'translate3d(0, -50%, 0)',
+            opacity: '0'
+          },
+          '100%': {
+            transform: 'none',
+            opacity: '1'
+          },
+        },
+
+        fadein: {
+          '0%': {
+            opacity: '0'
+          },
+          '100%': {
+            opacity: '0.5'
+          },
+        },
+
+      }
+    },
   },
   plugins: [],
 }
