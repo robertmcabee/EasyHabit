455da147767605b20a037578886ed366a8b57e95
Robert McAbee <robertthemcabee@gmail.com>
2022-04-28 10:16:23 -0600

    minor fix

--- src/components/DevelopmentButtons.js
+++ src/components/DevelopmentButtons.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class DevelopmentButtons extends Component {
   state = {};
   render() {
-    if (false) return null;
+    if (true) return null;
     return (
       <div className="mb-8 flex w-full justify-center space-x-2">
         <button
