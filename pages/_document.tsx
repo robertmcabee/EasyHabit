2319acb10e9822ad228bc9d0f7b0f6882acc07b0
Robert McAbee <robertthemcabee@gmail.com>
2022-06-11 15:05:07 -0600

    Safari visual fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,16 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="description" content="An Intuitive Habit Tracker" />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="rgb(250, 250, 250)"
+        />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: dark)"
+          content="rgb(38, 38, 38)"
+        />
         <link rel="icon" type="image/png" href="/favicon.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,400&display=swap"
